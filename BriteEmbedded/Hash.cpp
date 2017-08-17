64f2a42bb166bf9da7f13dac24eb86e9bddeb652
Eyaz Rehman <eyaz.rehman@uoit.net>
2017-08-16 23:53:33 -0400

    Use compile time hash function where needed.

--- BriteEmbedded/Hash.cpp
+++ BriteEmbedded/Hash.cpp
@@ -10,5 +10,5 @@ uint32_t FNV1A32(uint8_t *obj, uint32_t size, uint32_t prime, uint32_t offset) {
 }
 
 constexpr uint32_t FNV1ACompileTime32(uint8_t *obj, uint32_t last) {
-	return *obj ? FNV1A32(obj + 1, (*obj ^ last) * FNV1APrime32) : last;
+	return *obj ? FNV1ACompileTime32(obj + 1, (*obj ^ last) * FNV1APrime32) : last;
 }
